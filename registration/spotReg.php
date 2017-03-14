ce52232231c65819d5e0e28f28dfe435d30419f2
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-14 10:46:04 +0530

    intendation

--- registration/spotReg.php
+++ registration/spotReg.php
@@ -2,30 +2,26 @@
 require 'db_connect.php';
 session_create();
 if(session_check()==true)
- {
- if(session_get_reg()==1)
 {
-	while(1)
+	if(session_get_reg()==1)
 	{
-	$num=rand(1000,9999);
-    $email="invalid";
-    $x="1234567890qwertyuiopasdfghjklzxcvbnm";
-    for($i=0;$i<6;$i++)
-    {
-        $email.=$x[rand(0,strlen($x))];
-    }
-	
-	$sqlget="select phone from participants where phone='".$num."' or email='".$email."'";
-	$result=$con->query($sqlget);
-	if($result->num_rows==0)
-		break;
-
-}
-     $sqlins="INSERT INTO `participants` (`id`, `name`, `college`, `phone`, `gender`, `accommodation`, `email`) VALUES (NULL, 'invalid user', 'invalid college', '".$num."', 'mf', '-1', '".$email."');";
-	$con->query($sqlins)or die($sqlins);
-	header('Location:Registration.php?name='.$num.'&option=0');
-
-
-}
+		while(1)
+		{
+			$num=rand(1000,9999);
+		    $email="invalid";
+		    $x="1234567890qwertyuiopasdfghjklzxcvbnm";
+		    for($i=0;$i<6;$i++)
+		    {
+		        $email.=$x[rand(0,strlen($x))];
+		    }	
+			$sqlget="select phone from participants where phone='".$num."' or email='".$email."'";
+			$result=$con->query($sqlget);
+			if($result->num_rows==0)
+				break;	
+		}
+	    $sqlins="INSERT INTO `participants` (`id`, `name`, `college`, `phone`, `gender`, `accommodation`, `email`) VALUES (NULL, 'invalid user', 'invalid college', '".$num."', 'mf', '-1', '".$email."');";
+		$con->query($sqlins)or die($sqlins);
+		header('Location:Registration.php?name='.$num.'&option=0');
+	}
 }
 ?>
