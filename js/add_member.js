64d46da9a813178f85f227e1277c698827a16033
WAVIFICATION <jerin.jacob1@gmail.com>
2017-03-14 22:07:39 +0530

    updated addMember.php

--- js/add_member.js
+++ js/add_member.js
@@ -3,6 +3,9 @@ function Add_Member()
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+               // alert(xt);
+                window.opener.pageReload();
+                window.close();
             }
         };
    	xx=( window.document.getElementById("eid").innerHTML).replace(/[^0-9]/g, ''); 
