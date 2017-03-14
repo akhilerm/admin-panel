3e6b836e40dfeb44311b459171258e5d63dff86f
Akhil Mohan <akhilerm@gmail.com>
2017-03-14 15:10:36 +0530

    Coordinator aligned and session created

--- cordinator/first.php
+++ cordinator/first.php
@@ -1,13 +1,20 @@
 <?php
-//session_start();
-require_once("../db_connect.php");
-$event_id=1001;
-	//event id has to be get
-	$part_id=$_GET['id'];
-	
-$query = "UPDATE event_participants_spot set attendance = 1 where event_id = '".$event_id."' and part_id = '".$part_id."';";
-echo $query;	
-	$result = $con-> query($query);
-		
+	require_once("../db_connect.php");
+	session_create();
+	if(session_check()==true)
+  	{
+    	if($_SESSION['LoggedINCoord']==1)
+    	{
+			$event_id=1001;
+			//event id has to be get
+			$part_id=$_GET['id'];
+			$query = "UPDATE event_participants_spot set attendance = 1 where event_id = '".$event_id."' and part_id = '".$part_id."';";
+			echo $query;	
+			$result = $con-> query($query);
 			header("location:../index.php");
-			?>
+		}
+		else
+	}		header("location:../index.php");
+	else
+		header("location:../index.php");
+?>
--- cordinator/unlock.php
+++ cordinator/unlock.php
@@ -1,11 +1,18 @@
 <?php
 	require_once("../db_connect.php");
-	$event_id=1001;
-	//event id has to be get
-		$query = "UPDATE events SET lock_event =0 WHERE event_id='".$event_id."';";
-		$result = $con->query($query);
-		
-	
-	header("location:index.php");
-	
-	?>
+	session_create();
+	if(session_check()==true)
+  	{
+    	if($_SESSION['LoggedINCoord']==1)
+    	{
+			$event_id=1001;
+			//event id has to be get
+			$query = "UPDATE events SET lock_event =0 WHERE event_id='".$event_id."';";
+			$result = $con->query($query);
+			header("location:index.php");
+		}
+		else
+	}		header("location:../index.php");
+	else
+		header("location:../index.php");
+?>
