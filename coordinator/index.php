6dab29cb88a3b67d06cf7631d2ce80ce100e4647
Mohammed Basith <mhdbazi@gmail.com>
2017-03-15 20:23:20 +0530

    coordinator layout changed

--- coordinator/index.php
+++ coordinator/index.php
@@ -10,8 +10,9 @@
 			<html>
 				<head>
 					<title>Conjura'17</title>
-					<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
+				
 					<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+                    <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 					<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -19,17 +20,20 @@
 					<!--web-fonts-->
 					<!--<link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>-->
 					<script src="../js/jquery-1.12.0.min.js"></script>
-					<script src="../js/bootstrap.min.js"></script>
+					
 					<!--web-fonts-->
 				</head>
-				<body style="padding-bottom:3%;">
+				<body>
+                    <center>
+                    <?php
+                        include('../header.php');
+                        ?>
 					<!---header-->
-					<div class="header w3ls">
-						<h2 style="color: black;"><img src="../images/LOGO.png" height="90px" width="90px">CONJURA'17</h2>
-					</div>
+				
 					<div class="main-section agile">         
-						<nav>            
-		          			<ul>
+						   <div class="row"> 
+                               <div style="margin-bottom:20px;">
+		          			<button class="btn-eventdet btn btn-default" style="margin-top:100px;">
 		        			<?php 
 								$sql = "SELECT lock_event FROM events WHERE event_id='".$event_id."';";
 								$result = $con->query($sql);
@@ -37,16 +41,18 @@
 									while($row=$result->fetch_assoc())
 										$status=$row['lock_event'];
 								if($status == 0)
-									echo "<li><a href='lock.php' >LOCK</a></li>";
+									echo "<a href='lock.php' >LOCK</a>";
 								else
-									echo "<li><a href='unlock.php' >UNLOCK</a></li>";
+									echo "<a href='unlock.php' >UNLOCK</a>";
 							?>
-		        			</ul>
-		            	</nav>
-		   				<br/><br/><br/>
+		        			</button>
+                            </div>
+		            	<div>
 						<form action="index.php" >
-							<input type ="submit" value ="view"/>
+							<input  class="btn-eventdet btn btn-default" type ="submit" value ="VIEW"/>
 						</form>
+                        </div>
+                        </div>
 						<br/>
 						<?php 
 							$query="SELECT id,name,college,email,phone,attendance from participants as p,event_participants_spot as t where p.id = t.part_id and event_id='".$event_id."';";
@@ -54,9 +60,9 @@
 							$count=0;
 							if($result->num_rows>0)
 							{
-								echo "<table class='table table-striped'>";
-								echo "<tr>";
-								echo "<th>S.NO</th><th>CONJURA ID</th><th>NAME</th><th>COLLEGE</th><th>EMAIL</th><th>PHONE</th><th>attendance</th><th>prizes</th>";
+								echo "<table class='table-reg table table-bordered table-striped'>";
+								echo "<tr class='colblue'>";
+								echo "<th >S.NO</th><th>CONJURA ID</th><th>NAME</th><th>COLLEGE</th><th>EMAIL</th><th>PHONE</th><th>attendance</th><th>prizes</th>";
 								echo "</tr>";
 								while($row=$result->fetch_assoc())
 								{
@@ -83,8 +89,14 @@
 							}
 							else
 								echo "NONE HAS REGISTERED";
-							include ('../footer.php'); 
+							
 						?>
+                        <footer class="footer-relative">
+                          <div class="container">
+                            <p class="text-muted text-center">Conjura'17</p>
+                          </div>
+                        </footer>
+                        </center>
 					</div> 
 				</body>
 			</html>
