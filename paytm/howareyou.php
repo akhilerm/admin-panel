2d8755e641109e912832d4817d59a362dc34f34f
Pranav Shenoy <pranavshenoy06@gmail.com>
2017-03-17 21:47:05 +0530

    making howareyou.php

--- paytm/howareyou.php
+++ paytm/howareyou.php
@@ -1,46 +1,45 @@
 <?php
-    require_once("../db_connect.php");
-    session_create();
-    if(1)
-    {
-       if(1)
-        {
+    require('../db_connect.php');
+	session_create();
+	if(session_check()==true)
+	{
+		if(session_get_reg()==1)
+		{
 ?>
 <html>
 <head>
-            	
 		<title>Conjura'17</title>
-	
 		<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
-<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
+		<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<script src="../js/Registration.js"></script>
-		<!--web-fonts-->
-		<!--<link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>-->
 		<script src="../js/jquery-1.12.0.min.js"></script>
-		
-		<!--web-fonts-->
-		
 </head>
 <body>
 
 <?php 
-        $query="SELECT trans_id,amt from paytm where part_id=0";
+        $query="SELECT * from paytm";
         $result = $con->query($query);
         echo "<div class='content'>";
         echo "<table class='super-admin-table'>
 				  <tr>
-				    <th>Transaction ID</th>
+				  	<th>Transaction ID</th>
 				    <th>Amount</th>
+				    <th>Conjura_id</th>
+				    <th>Amount Difference</th>
+				    <th>Status</th>
 				  </tr>";
         if($result){
             
 			while($row = mysqli_fetch_array($result)) {
 			    echo "<tr>";
-			    echo "<td>" . $row[0] . "  </td>";
-			    echo "<td>" . $row[1] . "</td>";
+			    echo "<td>".$row['trans_id']."  </td>";
+			    echo "<td>".$row['amt']."</td>";
+			    echo "<td>".$row['part_id'] . "</td>";
+   			    echo "<td>".$row['amt_diff']."</td>";
+			    echo "<td>".$row['status']."</td>";
 			    echo "</tr>";
 			}
 			
